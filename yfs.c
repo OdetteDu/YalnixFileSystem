308cf79fe16206cbc41841eaf10e740a739fdb3f
OdetteDu <Odette.Du@rice.edu>
2014-04-11 13:54:25 -0500

    Print inode

--- yfs.c
+++ yfs.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <comp421/yalnix.h>
 #include <comp421/filesystem.h>
 #include <comp421/iolib.h>
@@ -14,7 +15,28 @@ int main(int argc, char **argv)
 	return 0;
 }
 
-void printInode(struct inode *inode)
+void printInode(int level, char *where, struct inode *inode)
 {
-	  
+	//Print basic information about an inode
+	TracePrintf(level, "[Testing @ %s]: inode(type: %d, nlink: %d, size: %d, indirect: %d)\n", where, inode ->type, inode ->nlink, inode->size, inode->indirect);
+	
+	//Print direct block
+	int i;
+	for(i=0; i<NUM_DIRECT; i++)
+	{
+		  TracePrintf(level, "%d ", inode ->direct[i]);
+	}
+
+	//Print indirect block
+	char *buf = malloc(sizeof(char) * SECTORSIZE);
+	int readIndirectBlockStatus;
+	readIndirectBlockStatus = ReadSector(inode ->indirect, buf);
+	if( readIndirectBlockStatus != 0)
+	{
+		TracePrintf(0, "[Error @ %s @ printInode]: Read indirect block unsuccessfully\n", where);
+	}
+
+
+
+	TracePrintf(level, "\n");
 }
