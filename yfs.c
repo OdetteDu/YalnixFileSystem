81e89c1b611da3d0cb956f7fb70067bc18b00efc
OdetteDu <Odette.Du@rice.edu>
2014-04-11 01:27:52 -0500

    register the yfs as File_server

--- yfs.c
+++ yfs.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <comp421/yalnix.h>
+#include <comp421/filesystem.h>
+#include <comp421/iolib.h>
 
 int main(int argc, char **argv)
 {
-	printf("Hello, World");
+	int registerStatus = Register(FILE_SERVER);
+	if(registerStatus != 0)
+	{
+		TracePrintf(0, "[Error @ yfs.main]: unsuccessfully register the YFS as the FILE_SERVER.\n");
+	}
+
 	return 0;
 }
