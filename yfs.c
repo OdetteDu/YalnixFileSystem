95d40f9f13396477e346ab613e74e19863108283
OdetteDu <Odette.Du@rice.edu>
2014-04-15 21:03:11 -0500

    in the middle of calculating free blocks

--- yfs.c
+++ yfs.c
@@ -92,6 +92,93 @@ void printDisk(int level, char *where)
 	TracePrintf(level, "\n");
 }
 
+void calculateFreeBlocksAndInodes()
+{
+	char *where = "yfs.c @ calculateFreeBlocksAndInodes()";
+	TracePrintf(1000, "[Testing @ %s]: Start:\n", where);
+
+	//Read block 1
+	char *buf;
+	buf = malloc(sizeof(char) * SECTORSIZE);
+	int readIndirectBlockStatus;
+	readIndirectBlockStatus = ReadSector(1, buf);
+	if (readIndirectBlockStatus != 0)
+	{
+		TracePrintf(0, "[Error @ %s @ printDisk]: Read indirect block %d unsuccessfully\n", where, 1);
+		return;
+	}
+
+	//Get total number of inodes and blocks from fs_header
+	struct fs_header *fsHeader = (struct fs_header *) buf;
+	int isInodeFree[fsHeader->num_inodes];
+	int isBlockFree[fsHeader->num_blocks];
+
+	//Print inodesi in blcok 1
+	int numOfBlocksContainingInodes = ((fsHeader->num_inodes) + 1) / (BLOCKSIZE / INODESIZE);
+	int i;
+	int j;
+	for (i = 1; i < BLOCKSIZE / INODESIZE - 1; i++)
+	{
+		struct *inode = (struct inode *) buf + i;
+
+		//Print basic information about an inode
+			TracePrintf(level, "[Testing @ %s]: inode(type: %d, nlink: %d, size: %d, indirect: %d)\n", where, inode->type, inode->nlink, inode->size,
+					inode->indirect);
+
+			//Print direct block
+			TracePrintf(level, "Direct block: \n");
+			int i;
+			for (i = 0; i < NUM_DIRECT; i++)
+			{
+				TracePrintf(level, "%d\n", inode->direct[i]);
+			}
+			TracePrintf(level, "\n");
+
+			//Print indirect block
+			if (inode->indirect != 0)
+			{
+				char *buf;
+				buf = malloc(sizeof(char) * SECTORSIZE);
+				int readIndirectBlockStatus;
+				readIndirectBlockStatus = ReadSector(inode->indirect, buf);
+				if (readIndirectBlockStatus != 0)
+				{
+					TracePrintf(0, "[Error @ %s @ printInode]: Read indirect block %d unsuccessfully\n", where, inode->indirect);
+				}
+				else
+				{
+					TracePrintf(level, "Indirect block: \n");
+					for (i = 0; i < sizeof(char) * SECTORSIZE / sizeof(int); i++)
+					{
+						TracePrintf(level, "%d\n", (int *) buf + i);
+					}
+				}
+			}
+
+			TracePrintf(level, "\n");
+	}
+
+	//Print inodes not in block 1
+	int blockIndex;
+	for (blockIndex = 2; blockIndex < numOfBlocksContainingInodes + 1; blockIndex++)
+	{
+		readIndirectBlockStatus = ReadSector(blockIndex, buf);
+		if (readIndirectBlockStatus != 0)
+		{
+			TracePrintf(0, "[Error @ %s @ printDisk]: Read indirect block %d unsuccessfully\n", where, blockIndex);
+		}
+		else
+		{
+			for (i = 0; i < BLOCKSIZE / INODESIZE - 1; i++)
+			{
+				printInode(level, "", (struct inode *) buf + i);
+			}
+		}
+	}
+
+	TracePrintf(level, "\n");
+}
+
 int main(int argc, char **argv)
 {
 	int registerStatus = Register(FILE_SERVER);
@@ -100,7 +187,7 @@ int main(int argc, char **argv)
 		TracePrintf(0, "[Error @ yfs.main]: unsuccessfully register the YFS as the FILE_SERVER.\n");
 	}
 
-	printDisk(1000, "main.c");
+	printDisk(1500, "main.c");
 
 	return 0;
 }
