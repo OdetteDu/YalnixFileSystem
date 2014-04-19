3ee2f1a6ba45a39a7e45b6049cde6f269d50edac
OdetteDu <Odette.Du@rice.edu>
2014-04-19 14:14:10 -0700

    Write getBlockUsed

--- yfs.c
+++ yfs.c
@@ -6,7 +6,7 @@
 
 #include "global.h"
 
-struct Message *msg; 
+struct Message *msg;
 char *pathname;
 char fileName[DIRNAMELEN];
 int fileNameCount;
@@ -172,19 +172,19 @@ void calculateFreeBlocksAndInodes()
 	//Get total number of inodes and blocks from fs_header
 	struct fs_header *fsHeader = (struct fs_header *) buf;
 	int numInodes = fsHeader->num_inodes;
-	int numBlocks = fsHeader->num_blocks; 
+	int numBlocks = fsHeader->num_blocks;
 	int *isInodeFree = malloc( sizeof(int) * numInodes );
 	int *isBlockFree = malloc( sizeof(int) * numBlocks );
 
 	int i;
-	TracePrintf( level, "[Testing @ %s]: Free Inodes %d:\n", where, numInodes);
+	TracePrintf( level, "[Testing @ %s]: Free Inodes %d:\n", where, numInodes );
 	for( i = 0; i < numInodes; i++ )
 	{
 		isInodeFree[i] = FREE;
 	}
 	TracePrintf( level, "\n" );
 
-	TracePrintf( level, "[Testing @ %s]: Free Blocks %d:\n", where, numBlocks);
+	TracePrintf( level, "[Testing @ %s]: Free Blocks %d:\n", where, numBlocks );
 	for( i = 0; i < numBlocks; i++ )
 	{
 		isBlockFree[i] = FREE;
@@ -193,7 +193,7 @@ void calculateFreeBlocksAndInodes()
 
 	//Print inodes in blcok 1
 	int numOfBlocksContainingInodes = ((fsHeader->num_inodes) + 1) / (BLOCKSIZE / INODESIZE);
-	TracePrintf( level, "BLOCKSIZE/INODESIZE: %d, inodes in %d blocks:\n", BLOCKSIZE/INODESIZE, numOfBlocksContainingInodes );
+	TracePrintf( level, "BLOCKSIZE/INODESIZE: %d, inodes in %d blocks:\n", BLOCKSIZE / INODESIZE, numOfBlocksContainingInodes );
 	int inodeIndex = 0;
 	for( i = 1; i < BLOCKSIZE / INODESIZE; i++ )
 	{
@@ -236,19 +236,105 @@ void calculateFreeBlocksAndInodes()
 		}
 	}
 
-	TracePrintf( level, "[Testing @ %s]: Free Inodes %d:\n", where, numInodes);
+	TracePrintf( level, "[Testing @ %s]: Free Inodes %d:\n", where, numInodes );
 	for( i = 0; i < numInodes; i++ )
 	{
 		TracePrintf( level, "%d:%d\n", i, isInodeFree[i] );
 	}
 	TracePrintf( level, "\n" );
 
-	TracePrintf( level, "[Testing @ %s]: Free Blocks %d:\n", where, numBlocks);
+	TracePrintf( level, "[Testing @ %s]: Free Blocks %d:\n", where, numBlocks );
 	for( i = 0; i < numBlocks; i++ )
 	{
 		TracePrintf( level, "%d:%d\n", i, isBlockFree[i] );
 	}
 	TracePrintf( level, "\n" );
+}
+
+int calculateNumBlocksUsed( int size )
+{
+	if( size % BLOCKSIZE == 0 )
+	{
+		return size / BLOCKSIZE;
+	}
+	else
+	{
+		return size / BLOCKSIZE;
+	}
+}
+
+//get a list of block number that a file is using
+int* getUsedBlocks( struct inode *inode )
+{
+	int level = 1400;
+	char *where = "yfs.c @ getUsedBlocks";
+
+	//Print basic information about an inode
+	TracePrintf( level, "[Testing @ %s]: inode(type: %d, nlink: %d, size: %d, indirect: %d)\n", where, inode->type, inode->nlink, inode->size,
+			inode->indirect );
+
+	if( inode->type == INODE_FREE )
+	{
+		TracePrintf( 0, "[Error @ %s @ printInode]: The inode you are trying to get used block is free\n", where );
+		return ERROR; //return 0
+	}
+
+	int count;
+	int numBlocks = calculateNumBlocksUsed( inode->size );
+	int *usedBlocks = (int *) malloc( sizeof(int) * numBlocks );
+
+	if( numBlocks <= NUM_DIRECT )
+	{
+		//only need to handle direct blocks
+		TracePrintf( level, "Direct block: \n" );
+		int i;
+		for( i = 0; i < numBlocks; i++ )
+		{
+			TracePrintf( level, "%d\n", inode->direct[i] );
+			usedBlocks[count] = inode->direct[i];
+			count++;
+		}
+		TracePrintf( level, "\n" );
+	}
+	else
+	{
+		//need to handle indirect blocks
+		TracePrintf( level, "Direct block: \n" );
+		int i;
+		for( i = 0; i < NUM_DIRECT; i++ )
+		{
+			TracePrintf( level, "%d\n", inode->direct[i] );
+			usedBlocks[count] = inode->direct[i];
+			count++;
+		}
+		TracePrintf( level, "\n" );
+
+		//Print indirect block
+		if( inode->indirect != 0 )
+		{
+			char *buf;
+			buf = malloc( sizeof(char) * SECTORSIZE );
+			int readIndirectBlockStatus;
+			readIndirectBlockStatus = ReadSector( inode->indirect, buf );
+			if( readIndirectBlockStatus != 0 )
+			{
+				TracePrintf( 0, "[Error @ %s @ printInode]: Read indirect block %d unsuccessfully\n", where, inode->indirect );
+				return ERROR;
+			}
+
+			TracePrintf( level, "Indirect block: \n" );
+			for( i = 0; i < numBlocks - NUM_DIRECT; i++ )
+			{
+				int blockIndex = *((int *) buf + i);
+				TracePrintf( level, "%d\n", blockIndex );
+				usedBlocks[count] = blockIndex;
+				count++;
+			}
+			TracePrintf( level, "\n" );
+
+		}
+	}
+
 }
 
 //return the last directory's inode number
@@ -258,76 +344,75 @@ int gotoDirectory( char *pathname, int pathNameLen )
 	int lastDirectoryInodeNum;
 	char c = pathname[0];
 	//check is pathname is empty
-	if(pathNameLen == 0 || c=='\0')
+	if( pathNameLen == 0 || c == '\0' )
 	{
-		TracePrintf(0, "[Error @ yfs.c @ gotoDirectory]: pathname's length is 0: %s\n", pathname);
+		TracePrintf( 0, "[Error @ yfs.c @ gotoDirectory]: pathname's length is 0: %s\n", pathname );
 		return ERROR;
 	}
 
 	//determine if the file use absolute or relative pathname
-	if(c=='/')
+	if( c == '/' )
 	{
 		//This is absolute pathname
-		TracePrintf(300, "[Testing @ yfs.c @ gotoDirectory]: absolute pathname: %s \n", pathname);
+		TracePrintf( 300, "[Testing @ yfs.c @ gotoDirectory]: absolute pathname: %s \n", pathname );
 		lastDirectoryInodeNum = ROOTINODE;
 	}
 	else
 	{
 		//This is relative pathname
-		TracePrintf(300, "[Testing @ yfs.c @ gotoDirectory]: relative pathname: %s \n", pathname);
+		TracePrintf( 300, "[Testing @ yfs.c @ gotoDirectory]: relative pathname: %s \n", pathname );
 	}
 
-
 	//parse the pathname and change the lastDirectoryInodeNum
-	for(i=0; i<MAXPATHNAMELEN; i++)
+	for( i = 0; i < MAXPATHNAMELEN; i++ )
 	{
 		c = pathname[i];
-		TracePrintf(1300, "[Testing @ yfs.c @ gotoDirectory]: char: %c\n", c);
+		TracePrintf( 1300, "[Testing @ yfs.c @ gotoDirectory]: char: %c\n", c );
 
-		if(c == '/')
+		if( c == '/' )
 		{
-			if(fileNameCount != 0)
+			if( fileNameCount != 0 )
 			{
-				TracePrintf(300, "[Testing @ yfs.c @ gotoDirectory]: need to go to this directory (%s) in the current directory\n", fileName);
+				TracePrintf( 300, "[Testing @ yfs.c @ gotoDirectory]: need to go to this directory (%s) in the current directory\n", fileName );
 				fileNameCount = 0;
-				for(j=0; j<DIRNAMELEN; j++)
+				for( j = 0; j < DIRNAMELEN; j++ )
 				{
 					fileName[j] = 0;
 				}
 			}
 		}
-		else if(c == '\0')
+		else if( c == '\0' )
 		{
-			TracePrintf(300, "[Testing @ yfs.c @ gotoDirectory]: parse char 0\n");
+			TracePrintf( 300, "[Testing @ yfs.c @ gotoDirectory]: parse char 0\n" );
 			break;
 		}
 		else
 		{
 			fileName[fileNameCount] = c;
-			fileNameCount ++;
-			if(fileNameCount >= DIRNAMELEN)
+			fileNameCount++;
+			if( fileNameCount >= DIRNAMELEN )
 			{
-				TracePrintf(0, "[Error @ yfs.c @ gotoDirectory]: file name length exceeds DIRNAMELEN: %s\n", fileName);
+				TracePrintf( 0, "[Error @ yfs.c @ gotoDirectory]: file name length exceeds DIRNAMELEN: %s\n", fileName );
 			}
 		}
 	}
 
-	TracePrintf(300, "[Testing @ yfs.c @ gotoDirectory]: need to go to open or create this file (%s) in the current directory\n", fileName);
+	TracePrintf( 300, "[Testing @ yfs.c @ gotoDirectory]: need to go to open or create this file (%s) in the current directory\n", fileName );
 
 	return lastDirectoryInodeNum;
 }
 
-void readDirectory(int inodeNum)
+void readDirectory( int inodeNum )
 {
-	  
+
 }
 
-int createFile(char *pathname, int pathNameLen)
+int createFile( char *pathname, int pathNameLen )
 {
-	int directoryInodeNum = gotoDirectory(pathname, pathNameLen);
-	if(directoryInodeNum == ERROR)
+	int directoryInodeNum = gotoDirectory( pathname, pathNameLen );
+	if( directoryInodeNum == ERROR )
 	{
-		TracePrintf(0, "[Error @ yfs.c @ Create]: directoryInodeNum is Error: pathname: %s fileName: %s\n", pathname, fileName);
+		TracePrintf( 0, "[Error @ yfs.c @ Create]: directoryInodeNum is Error: pathname: %s fileName: %s\n", pathname, fileName );
 		return ERROR;
 	}
 
@@ -335,10 +420,10 @@ int createFile(char *pathname, int pathNameLen)
 	return 0;
 }
 
-void addressMessage(int pid, struct Message *msg)
+void addressMessage( int pid, struct Message *msg )
 {
-	int type = msg -> messageType;
-	TracePrintf(500, "[Testing @ yfs.c @ receiveMessage]: receive message typed %d\n", type);
+	int type = msg->messageType;
+	TracePrintf( 500, "[Testing @ yfs.c @ receiveMessage]: receive message typed %d\n", type );
 
 	int len;
 //	char *pathname;
@@ -347,94 +432,87 @@ void addressMessage(int pid, struct Message *msg)
 	int inode;
 	int currentPos;
 
-	if( type == OPEN || 
-		type == CREATE ||
-		type == UNLINK || 
-		type == MKDIR ||
-		type == RMDIR ||
-		type == CHDIR ||
-		type == STAT ||
-		type == READLINK ||
-		type == LINK ||
-		type == SYMLINK )
+	if( type == OPEN || type == CREATE || type == UNLINK || type == MKDIR || type == RMDIR || type == CHDIR || type == STAT || type == READLINK
+			|| type == LINK || type == SYMLINK )
 	{
 		//get pathname and len
-		len = msg -> len;
+		len = msg->len;
 //		pathname = malloc(sizeof(char) * len);
-		int copyFrom = CopyFrom(pid, pathname, msg -> pathname, len);
-		if(copyFrom != 0)
+		int copyFrom = CopyFrom( pid, pathname, msg->pathname, len );
+		if( copyFrom != 0 )
 		{
-			TracePrintf(0, "[Error @ yfs.c @ addressMessage]: copy pathname from pid %d failure\n", pid);
+			TracePrintf( 0, "[Error @ yfs.c @ addressMessage]: copy pathname from pid %d failure\n", pid );
 		}
 	}
 
-	if( type == READ ||
-		type == WRITE ||
-		type == LINK ||
-		type == SYMLINK ||
-		type == READLINK)
+	if( type == READ || type == WRITE || type == LINK || type == SYMLINK || type == READLINK )
 	{
 		//get buf and size
-		size = msg -> size;
-		buf = malloc(sizeof(char) * size);
-		int copyFrom = CopyFrom(pid, buf, msg -> buf, size);
-		if(copyFrom != 0)
+		size = msg->size;
+		buf = malloc( sizeof(char) * size );
+		int copyFrom = CopyFrom( pid, buf, msg->buf, size );
+		if( copyFrom != 0 )
 		{
-			TracePrintf(0, "[Error @ yfs.c @ addressMessage]: copy buf from pid %d failure\n", pid);
+			TracePrintf( 0, "[Error @ yfs.c @ addressMessage]: copy buf from pid %d failure\n", pid );
 		}
 	}
 
 	if( type == READ || type == WRITE )
 	{
 		//get inode and pos
-		inode = msg -> inode;
-		currentPos = msg -> len;
+		inode = msg->inode;
+		currentPos = msg->len;
 	}
 
-	switch(type)
+	switch( type )
 	{
 		case OPEN:
-			TracePrintf(500, "[Testing @ yfs.c @ addressMessage]: Message OPEN: type(%d), len(%d), pathname(%s)\n", type, len, pathname);
+			TracePrintf( 500, "[Testing @ yfs.c @ addressMessage]: Message OPEN: type(%d), len(%d), pathname(%s)\n", type, len, pathname );
 			break;
 		case CREATE:
-			createFile(pathname, len);
-			TracePrintf(500, "[Testing @ yfs.c @ addressMessage]: Message CREATE: type(%d), len(%d), pathname(%s)\n", type, len, pathname);
+			createFile( pathname, len );
+			TracePrintf( 500, "[Testing @ yfs.c @ addressMessage]: Message CREATE: type(%d), len(%d), pathname(%s)\n", type, len, pathname );
 			break;
 		case UNLINK:
-			TracePrintf(500, "[Testing @ yfs.c @ addressMessage]: Message UNLINK: type(%d), len(%d), pathname(%s)\n", type, len, pathname);
+			TracePrintf( 500, "[Testing @ yfs.c @ addressMessage]: Message UNLINK: type(%d), len(%d), pathname(%s)\n", type, len, pathname );
 			break;
 		case MKDIR:
-			TracePrintf(500, "[Testing @ yfs.c @ addressMessage]: Message MKDIR: type(%d), len(%d), pathname(%s)\n", type, len, pathname);
+			TracePrintf( 500, "[Testing @ yfs.c @ addressMessage]: Message MKDIR: type(%d), len(%d), pathname(%s)\n", type, len, pathname );
 			break;
 		case RMDIR:
-			TracePrintf(500, "[Testing @ yfs.c @ addressMessage]: Message RMDIR: type(%d), len(%d), pathname(%s)\n", type, len, pathname);
+			TracePrintf( 500, "[Testing @ yfs.c @ addressMessage]: Message RMDIR: type(%d), len(%d), pathname(%s)\n", type, len, pathname );
 			break;
 		case CHDIR:
-			TracePrintf(500, "[Testing @ yfs.c @ addressMessage]: Message CHDIR: type(%d), len(%d), pathname(%s)\n", type, len, pathname);
+			TracePrintf( 500, "[Testing @ yfs.c @ addressMessage]: Message CHDIR: type(%d), len(%d), pathname(%s)\n", type, len, pathname );
 			break;
 		case STAT:
-			TracePrintf(500, "[Testing @ yfs.c @ addressMessage]: Message STAT: type(%d), len(%d), pathname(%s)\n", type, len, pathname);
+			TracePrintf( 500, "[Testing @ yfs.c @ addressMessage]: Message STAT: type(%d), len(%d), pathname(%s)\n", type, len, pathname );
 			break;
 		case READLINK:
-			TracePrintf(500, "[Testing @ yfs.c @ addressMessage]: Message READLINK: type(%d), len(%d), pathname(%s), size(%d), buf(%s)\n", type, len, pathname, size, buf);
+			TracePrintf( 500, "[Testing @ yfs.c @ addressMessage]: Message READLINK: type(%d), len(%d), pathname(%s), size(%d), buf(%s)\n", type, len,
+					pathname, size, buf );
 			break;
 		case LINK:
-			TracePrintf(500, "[Testing @ yfs.c @ addressMessage]: Message LINK: type(%d), oldLen(%d), oldName(%s), newLen(%d), newName(%s)\n", type, len, pathname, size, buf);
+			TracePrintf( 500, "[Testing @ yfs.c @ addressMessage]: Message LINK: type(%d), oldLen(%d), oldName(%s), newLen(%d), newName(%s)\n", type,
+					len, pathname, size, buf );
 			break;
 		case SYMLINK:
-			TracePrintf(500, "[Testing @ yfs.c @ addressMessage]: Message SYMLINK: type(%d), oldLen(%d), oldName(%s), newLen(%d), newName(%s)\n", type, len, pathname, size, buf);
+			TracePrintf( 500, "[Testing @ yfs.c @ addressMessage]: Message SYMLINK: type(%d), oldLen(%d), oldName(%s), newLen(%d), newName(%s)\n",
+					type, len, pathname, size, buf );
 			break;
 		case READ:
-			TracePrintf(500, "[Testing @ yfs.c @ addressMessage]: Message READ: type(%d), inode(%d), pos(%d), size(%d), buf(%s)\n", type, inode, currentPos, size, buf);
+			TracePrintf( 500, "[Testing @ yfs.c @ addressMessage]: Message READ: type(%d), inode(%d), pos(%d), size(%d), buf(%s)\n", type, inode,
+					currentPos, size, buf );
 			break;
 		case WRITE:
-			TracePrintf(500, "[Testing @ yfs.c @ addressMessage]: Message WRITE: type(%d), inode(%d), pos(%d), size(%d), buf(%s)\n", type, inode, currentPos, size, buf);
+			TracePrintf( 500, "[Testing @ yfs.c @ addressMessage]: Message WRITE: type(%d), inode(%d), pos(%d), size(%d), buf(%s)\n", type, inode,
+					currentPos, size, buf );
 			break;
 		case SYNC:
-			TracePrintf(500, "[Testing @ yfs.c @ addressMessage]: Message SYNC: type(%d)\n", type);
+			TracePrintf( 500, "[Testing @ yfs.c @ addressMessage]: Message SYNC: type(%d)\n", type );
 			break;
 		case SHUTDOWN:
-			TracePrintf(500, "[Testing @ yfs.c @ addressMessage]: Message SHUTDOWN: type(%d)\n", type);
+			TracePrintf( 500, "[Testing @ yfs.c @ addressMessage]: Message SHUTDOWN: type(%d)\n", type );
 			break;
 	}
 }
@@ -451,29 +529,29 @@ int main( int argc, char **argv )
 	calculateFreeBlocksAndInodes();
 
 	int pid = Fork();
-	if(pid == 0)
+	if( pid == 0 )
 	{
-		Exec(argv[1], argv+1);
+		Exec( argv[1], argv + 1 );
 	}
 	else
 	{
-		msg = malloc(sizeof(struct Message));
-		pathname = malloc(sizeof(char) * MAXPATHNAMELEN);
-		while(1)
+		msg = malloc( sizeof(struct Message) );
+		pathname = malloc( sizeof(char) * MAXPATHNAMELEN );
+		while( 1 )
 		{
-			TracePrintf(500, "loop\n");
-			int sender = Receive(msg);
-			if(sender == ERROR)
+			TracePrintf( 500, "loop\n" );
+			int sender = Receive( msg );
+			if( sender == ERROR )
 			{
-				TracePrintf(0, "[Error @ yfs.c @ main]: Receive Message Failure\n");
+				TracePrintf( 0, "[Error @ yfs.c @ main]: Receive Message Failure\n" );
 				return ERROR;
 			}
-			
-			TracePrintf(500, "Sender: %d\n", sender);
-			addressMessage(sender, msg);
-			Reply(msg, sender);
+
+			TracePrintf( 500, "Sender: %d\n", sender );
+			addressMessage( sender, msg );
+			Reply( msg, sender );
 		}
 	}
-	
+
 	return 0;
 }
